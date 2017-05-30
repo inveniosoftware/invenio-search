6396d94b5e47598151e3b9201abac0d057dfb460
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2017-05-30 15:47:39 +0200

    tests: doctext execution simplification
    * Performs doctest execution within the initial pytest run, simplifying
      test suite execution and solving "disappearing development
      requirements" problem that was sometimes observed with
      development-level dependencies. (closes #95)
    
    * Removes unnecessary Flask-Security dependency.
    
    Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,9 @@ author = u'CERN'
 
 # Get the version string. Cannot be done with import!
 g = {}
-with open(os.path.join('..', 'invenio_search', 'version.py'), 'rt') as fp:
+with open(os.path.join(os.path.dirname(__file__), '..',
+                       'invenio_search', 'version.py'),
+          'rt') as fp:
     exec(fp.read(), g)
     version = g['__version__']
 

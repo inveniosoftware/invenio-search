8ae128865edd50ea45bf70e8afad577b31c2305b
Nicola Tarocco <nicola.tarocco@cern.ch>
2018-06-18 11:44:28 +0200

    installation: urllib3 dependency version fix

--- setup.py
+++ setup.py
@@ -71,8 +71,8 @@ install_requires = [
     # requests has hard version range dependency on idna
     # Every time idna and urllib3 are updated, installation breaks because
     # elasticsearch and requests dependencies are not resolved properly.
-    'urllib3<1.23,>=1.21.1',  # from elasticsearch and requests
-    'idna>=2.5,<2.8', # from requests
+    'urllib3<1.24,>=1.21.1',  # from elasticsearch and requests
+    'idna>=2.5,<2.8',   # from requests
 ]
 
 packages = find_packages()

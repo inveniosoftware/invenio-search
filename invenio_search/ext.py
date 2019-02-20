b56dcc7d4d4fa051c8b6701edf14379a50befadb
Rokas Maciulaitis <rokas.maciulaitis@cern.ch>
2019-02-20 16:19:36 +0100

    ext: adding deprecation warning for Elasticsearch v2
    * Closes #157

--- invenio_search/ext.py
+++ invenio_search/ext.py
@@ -62,6 +62,11 @@ class _SearchState(object):
         if entry_point_group_mappings:
             self.load_entry_point_group_mappings(entry_point_group_mappings)
 
+        if ES_VERSION[0] == 2:
+            warnings.warn(
+                "Elasticsearch v2 support will be removed.",
+                DeprecationWarning)
+
     @cached_property
     def templates(self):
         result = None

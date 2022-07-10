a2ed2b0b106b3c5165fcbea049fd9db787c1dcc8
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-07-10 12:52:46 +0200

    tests: allow warnings when building docs

--- run-tests.sh
+++ run-tests.sh
@@ -22,7 +22,7 @@ trap cleanup EXIT
 
 
 python -m check_manifest
-python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m sphinx.cmd.build -qnN docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --env)"
 python -m pytest
 tests_exit_code=$?
